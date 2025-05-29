f12b7068673c674cca066e2165ba802fb67872dd
John-mejia <jmejia@innerconsulting.com>
2025-05-28 22:48:54 -0500

    first commit

---
+++ app/src/main/java/com/uber/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.uber
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+
+    }
+}
